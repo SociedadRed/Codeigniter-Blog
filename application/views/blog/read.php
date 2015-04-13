d76d0d2cad8a13bdc4af3eb2c58b47c8b1cca2c2
Lenin Meza <lenin.meza@adapare.com>
2015-04-13 09:16:22 -0500

    Cambio en lenguaje

--- application/views/blog/read.php
+++ application/views/blog/read.php
@@ -8,5 +8,5 @@
     <?php echo anchor('blog/update/'.$post->id, 'Update'); ?>&nbsp
     <?php echo anchor('blog/delete/'.$post->id, 'Delete', 
         array('onclick' => "return confirm
-            ('Are you sure want to delete this post?')")); ?>
+            ('Seguro de querer borrar el post')")); ?>
 </p>
--- application/views/blog/update.php
+++ application/views/blog/update.php
@@ -1,13 +1,13 @@
 <?php echo form_open($action) ;?>
  
     <p>   
-        <label for="title">Title</label><br />
+        <label for="title">T&iacute;tulo</label><br />
         <input id="title" name="title" type="text"
             size="75" value="<?php echo $title; ?>" />
     </p>
  
     <p>
-        <label for="content">Content</label><br />
+        <label for="content">Contenido</label><br />
         <textarea id="content" name="content"
             ><?php echo $content; ?></textarea>  
     </p>
